bf607f5a9b23e87d89a53293ef0579183369d1dd
Takashi Nishibayashi <takashi_nishibayashi@voyagegroup.com>
2014-02-27 22:16:50 +0900

    Add launch script

---
+++ apns-proxy-server.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+app=`basename $0`
+pid=`dirname $0`/run/apns-proxy-server.pid
+
+
+start() {
+    if [ -f $pid ] && kill -0 `cat $pid` 2>/dev/null; then
+        echo "running already. pid: `cat $pid`";
+        return 1;
+    fi
+
+    cd `dirname $0`
+    exec 8> >(/usr/sbin/cronolog -w /var/log/app_preserve/$logdir/%Y/%m/%d/apns_proxy.log)
+    ./bin/python -m apns-proxy-server.invoker 2>&8 1>&8 &
+
+    mkdir -p `dirname $pid`
+    echo $! > $pid
+}
+
+stop() {
+    kill `cat $pid`
+    rm -f $pid
+    exec 8>&-
+}
+
+restart() {
+    stop
+    start
+}
+
+status() {
+    if [ -f $pid ]; then
+        ps u --pid=`cat $pid`
+        echo
+        ps xfo "%U %p %C %z %t (%x) %c : " o args=ARGS
+    else
+        echo "not running.";
+        exit 3;
+    fi
+}
+
+case "$1" in
+  start)
+    start
+    ;;
+  stop)
+    stop
+    ;;
+  restart)
+    restart
+    ;;
+  status)
+    status
+    ;;
+  *)
+    echo $"Usage: $0 {start|stop|restart|status}"
+    exit 1
+esac
+
+exit $?
