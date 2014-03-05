5aeca2ff560a293e2117ace07334706fdc1f8afb
Takashi Nishibayashi <takashi_nishibayashi@voyagegroup.com>
2014-03-05 17:16:43 +0900

    Fixed #1

--- apns_proxy_server/worker.py
+++ apns_proxy_server/worker.py
@@ -24,11 +24,16 @@ class SendWorkerThread(threading.Thread):
     """
     APNs送信用のワーカースレッド
     """
-    def __init__(self, queue, name, use_sandbox, cert_file, key_file):
+
+    # 送信済みのアイテムを保持する数, APNsからはエラーが非同期で得られるので
+    # リトライ用用に送信後しばらくは保持しておく必要がある
+    KEEP_SENDED_ITEMS_NUM = 2000
+
+    def __init__(self, task_queue, name, use_sandbox, cert_file, key_file):
         threading.Thread.__init__(self)
         self.setDaemon(True)
 
-        self.queue = queue
+        self.task_queue = task_queue
         self.name = name
 
         self.use_sandbox = use_sandbox
@@ -39,17 +44,12 @@ class SendWorkerThread(threading.Thread):
         self.count = 0
         # どのトークンがエラーになったか後で確認するための辞書
         self.recent_sended = {}
-        # 定期的にエラーレスポンスをチェックするためのタイムスタンプ
-        self.last_error_checked_time = time.time()
         # 一定時間送信しない場合は、APNsサーバーとの接続を切るためのタイムスタンプ
         self.last_sended_time = time.time()
 
     @property
     def apns(self):
         if self._apns is None:
-            logging.info('Get APNs Instance')
-            logging.info(self.cert_file)
-            logging.info(self.key_file)
             self._apns = APNs(
                 use_sandbox=self.use_sandbox,
                 cert_file=self.cert_file,
@@ -62,19 +62,50 @@ class SendWorkerThread(threading.Thread):
 
     def run(self):
         while True:
-            item = self.queue.get()
-            logging.debug("%s %s" % (self.name, item))
-            self.count += 1
-            self.recent_sended[self.count] = item['token']
-            self.send(item['token'], item.get('aps'), item.get('test'))
-            self.error_check()
+            try:
+                while True:
+                    self.main()
+            except socket.error, e:
+                if isinstance(e.args, tuple):
+                    logging.warn("errno is %s" % str(e[0]))
+                logging.warn(e)
+                # 考えられるエラー
+                # (1) 不正なトークンを送ったことにより、接続を切られた
+                # (2) コネクションを長く張りすぎた事により、接続を切られた
+                # どちらにしろ、どこまで送信成功したか判断できないので最後の一個だけリトライする
+                self.retry_last_one()
+            finally:
+                self.clear_connection()
+
+    def main(self):
+        item = self.task_queue.get()
+        #logging.debug("%s %s" % (self.name, item))
+        self.count += 1
+        self.push_recent_sended(self.count, item)
+        self.send(item['token'], item.get('aps'), item.get('test'))
+        self.error_check()
+
+    def push_recent_sended(self, idx, item):
+        self.recent_sended[idx] = item
+        if idx > self.KEEP_SENDED_ITEMS_NUM:
+            self.recent_sended.pop(idx - self.KEEP_SENDED_ITEMS_NUM)
 
     def send(self, token, aps, test=False):
         if test is True:
             return
+        logging.debug('Send %s' % token)
         self.apns.gateway_server.send_notification_multiple(
             self.create_frame(token, self.count, **aps)
         )
+
+    def retry_last_one(self):
+        self.retry_from(self.count)
+
+    def retry_from(self, start_token_idx):
+        idx = start_token_idx
+        while idx <= self.count:
+            self.task_queue.put(self.recent_sended[idx])
+            idx += 1
 
     def create_frame(self, token, identifier, alert, sound, badge, expiry):
         payload = Payload(alert=alert, sound=sound, badge=badge)
@@ -86,24 +117,22 @@ class SendWorkerThread(threading.Thread):
         return frame
 
     def error_check(self):
-        now_time = time.time()
-        if (self.count % 500 == 0) or (now_time - self.last_error_checked_time > 2):
+        if self.task_queue.empty() or (self.count % 500 == 0):
             try:
-                logging.info('%s Check error response %i' % (self.name, self.count))
+                logging.debug('%s Check error response %i' % (self.name, self.count))
                 self.check_apns_error_response()
             except APNsError, ape:
-                # 不正なトークン、APNsからは接続が切られるので、再接続して続行する
                 logging.warn(ape.msg)
-                logging.warn("Invalid token found %s", self.recent_sended[ape.token_idx])
+                # 不正なトークン、リモートサーバーからは接続が切られるので、再接続する
                 self.clear_connection()
-            except socket.error, e:
-                if isinstance(e.args, tuple):
-                    logging.warn("errno is %s" % str(e[0]))
-                logging.warn(e)
-                self.clear_connection()
-            finally:
-                self.recent_sended = {}
-                self.last_error_checked_time = time.time()
+                if ape.token_idx in self.recent_sended:
+                    # 不正なトークン以降に送った物は、送信できていないので再送する
+                    logging.warn("Invalid token found %s", self.recent_sended[ape.token_idx]['token'])
+                    self.retry_from(ape.token_idx + 1)
+                else:
+                    # recent_sendedから消した過去のtokenがエラーとして得られた場合
+                    # どうにもできない
+                    pass
 
     def check_apns_error_response(self):
         """
@@ -122,7 +151,6 @@ class SendWorkerThread(threading.Thread):
                 return
 
             # エラー有り
-            logging.info("Error response %s" % b2a_hex(error_bytes))
             command = b2a_hex(unpack('>c', error_bytes[0:1])[0])
             if command != '08':
                 logging.warn('Unknown command received %s', command)
