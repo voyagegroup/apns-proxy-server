4fe779a945fed58083367ef6a581f7d9facc67f6
Takashi Nishibayashi <takashi.nishibayashi@gmail.com>
2014-03-07 15:34:19 +0900

    Mod example

--- tests/connection_keep_test.py
+++ tests/connection_keep_test.py
@@ -10,17 +10,16 @@ def main():
     with client:
         token = "b7ae2fcdb2d325a2de86d572103bff6dd272576d43677544778c43a674407ecc"
         i = 0
-        interval = 3
+        interval = 120
         while i < 1000:
             i += 1
             client.send(token, u"☆この案件ありますお☆" + str(i), badge=i)
             print("Send %i" % i)
-            if interval < 10 * 60:
-                interval += 5
+            if interval < 20 * 60:
+                interval = int(interval * 1.4)
             time.sleep(interval)
 
 
-    print("Out of with")
-
 if __name__ == "__main__":
     main()
+    print("Done")
