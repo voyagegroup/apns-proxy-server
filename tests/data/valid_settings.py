4e6dc60793709111fed57cda4f1bd57f20aa330a
Takashi Nishibayashi <takashi_nishibayashi@voyagegroup.com>
2014-03-10 21:56:36 +0900

    サーバーのテストコードを追加

--- tests/data/valid_settings.py
+++ tests/data/valid_settings.py
@@ -6,9 +6,9 @@ import os.path
 LOG_LEVEL = logging.INFO
 
 # クライアントを待ちうけるポート
-BIND_PORT_FOR_ENTRY = 5556
+BIND_PORT_FOR_ENTRY = 4556
 # PUSH-PULL用のポート
-BIND_PORT_FOR_PULL = 5557
+BIND_PORT_FOR_PULL = 4557
 
 # アプリ毎のワーカースレッドの数
 THREAD_NUMS_PER_APPLICATION = 5
---
+++ tests/test_server.py
@@ -0,0 +1,132 @@
+# -*- coding: utf-8 -*-
+"""
+validatorモジュールのテスト
+"""
+
+import json
+import threading
+from Queue import Queue, Empty
+
+from nose.tools import ok_, eq_, raises
+
+from apns_proxy_server.server import (
+    COMMAND_ASK_ADDRESS,
+    COMMAND_SEND,
+    APNSProxyServer)
+
+
+def test_instance():
+    import tests.data.valid_settings
+    server = APNSProxyServer(tests.data.valid_settings)
+    ok_(server)
+    ok_(server.start)
+
+
+def test_create_worker():
+    import tests.data.valid_settings
+    server = APNSProxyServer(tests.data.valid_settings)
+    server.create_workers([{
+        "application_id": "myApp1",
+        "name": "My App1",
+        "sandbox": False,
+        "cert_file": "sample.cert",
+        "key_file": "sample.key"
+    }, {
+        "application_id": "myApp2",
+        "name": "My App2",
+        "sandbox": False,
+        "cert_file": "sample.cert",
+        "key_file": "sample.key"
+    }], 1)
+    eq_(len(server.task_queues), 2)
+    for k in server.task_queues.keys():
+        ok_(isinstance(server.task_queues[k], Queue))
+
+
+def test_dispatch_known_app():
+    import tests.data.valid_settings
+    server = APNSProxyServer(tests.data.valid_settings)
+    server.create_workers([{
+        "application_id": "myApp1",
+        "name": "My App1",
+        "sandbox": False,
+        "cert_file": "sample.cert",
+        "key_file": "sample.key"
+    }, {
+        "application_id": "myApp2",
+        "name": "My App2",
+        "sandbox": False,
+        "cert_file": "sample.cert",
+        "key_file": "sample.key"
+    }], 1)
+
+    token = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+    eq_(server.dispatch_queue(json.dumps({
+        "token": token,
+        "appid": "myApp1",
+        "aps": {
+            "alert": "This is test",
+            "badge": 1,
+            "sound": "default",
+            "expiry": None
+        }
+    })), True, "Dispatch should be success")
+
+    eq_(server.dispatch_queue(json.dumps({
+        "token": token,
+        "appid": "myApp2",
+        "aps": {
+            "alert": "This is test",
+            "badge": 1,
+            "sound": "default",
+            "expiry": None
+        }
+    })), True, "Dispatch should be success")
+
+
+def test_dispatch_unknown_app():
+    import tests.data.valid_settings
+    server = APNSProxyServer(tests.data.valid_settings)
+    server.create_workers([{
+        "application_id": "myApp2",
+        "name": "My App2",
+        "sandbox": False,
+        "cert_file": "sample.cert",
+        "key_file": "sample.key"
+    }], 1)
+
+    token = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+    eq_(server.dispatch_queue(json.dumps({
+        "token": token,
+        "appid": "unknownApp",
+        "aps": {
+            "alert": "This is test",
+            "badge": 1,
+            "sound": "default",
+            "expiry": None
+        }
+    })), False, "Dispatch should be failed of unknown appid")
+
+
+def test_start():
+    """
+    サーバーが起動できる事のテスト
+    """
+    def server(error_queue):
+        try:
+            import tests.data.valid_settings
+            server = APNSProxyServer(tests.data.valid_settings)
+            server.start()
+        except Exception, e:
+            error_queue.put(e)
+
+    q = Queue()
+    thread = threading.Thread(target=server,args=(q,))
+    thread.setDaemon(True)
+    thread.start()
+
+    try:
+        error = q.get(True, 1)
+        raise error
+    except Empty:
+        ok_(True)
